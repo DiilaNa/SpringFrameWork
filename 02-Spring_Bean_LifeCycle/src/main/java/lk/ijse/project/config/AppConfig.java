3c7e21030726819c967918dffc41de113655d0e8
DiilaNa <liyanaarachchidilan@gmail.com>
2025-06-19 10:56:53 +0530

    Scope type prototype beand are not added in to application context

--- 02-Spring_Bean_LifeCycle/src/main/java/lk/ijse/project/config/AppConfig.java
+++ 02-Spring_Bean_LifeCycle/src/main/java/lk/ijse/project/config/AppConfig.java
@@ -1,14 +1,17 @@
 package lk.ijse.project.config;
 
 import lk.ijse.project.bean.MyConnection;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.project.bean")
 public class AppConfig {
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public MyConnection myConnection() {
         return new MyConnection();
     }
