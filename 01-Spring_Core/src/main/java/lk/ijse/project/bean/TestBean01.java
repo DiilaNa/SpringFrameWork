2d96c55df8c7e24c193d1cb2f5b967dec53fb501
DiilaNa <liyanaarachchidilan@gmail.com>
2025-06-19 09:32:58 +0530

    Singleton behaviour changed using Scope annotation

--- 01-Spring_Core/src/main/java/lk/ijse/project/bean/TestBean01.java
+++ 01-Spring_Core/src/main/java/lk/ijse/project/bean/TestBean01.java
@@ -1,8 +1,11 @@
 package lk.ijse.project.bean;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) /*singleton behaviour is changed / two different ids for TestBeans*/
 public class TestBean01 {
     public TestBean01() {
         System.out.println("TestBean01 Constructor");
--- 01-Spring_Core/src/main/java/lk/ijse/project/config/AppConfig.java
+++ 01-Spring_Core/src/main/java/lk/ijse/project/config/AppConfig.java
@@ -1,14 +1,17 @@
 package lk.ijse.project.config;
 
 import lk.ijse.project.bean.MyConnection;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.project.bean")/*tell where thr beans are*/
 public class AppConfig {
     @Bean("getConnection")
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)/*singleton behaviour is changed/two bean ids*/
     MyConnection myConnection(){
         return new MyConnection();
     }
