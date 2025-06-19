ebf576201933df2964c1e772017c381d58ecef06
DiilaNa <liyanaarachchidilan@gmail.com>
2025-06-19 10:51:03 +0530

    Bean Life Cycle

--- 02-Spring_Bean_LifeCycle/src/main/java/lk/ijse/project/bean/MyConnection.java
+++ 02-Spring_Bean_LifeCycle/src/main/java/lk/ijse/project/bean/MyConnection.java
@@ -1,21 +1,38 @@
 package lk.ijse.project.bean;
 
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class MyConnection implements DisposableBean, BeanNameAware {
+public class MyConnection implements DisposableBean, BeanNameAware , BeanFactoryAware , ApplicationContextAware ,InitializingBean {
     /*The state of instantiation*/
 
     public MyConnection() {
-        System.out.println("MyConnection constructor");
+        System.out.println("MyConnection constructor is called");
     }
 
     /*There is no method to find the state of populate properties*/
-
     /*bean name aware*/
+
     @Override
     public void setBeanName(String name) {
-        System.out.println("setBeanName");
+        System.out.println("setBeanName is called");
+    }
+    /* bean Factory aware - dependency injections are added to bean*/
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("setBeanFactory is Called");
+    }
+    /*Aspect oriented programming and declarative programming satisfied*/
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("setApplicationContext is called");
+    }
+    /*Bean is ready to use*/
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet is called/Bean is ready to use");
     }
 
     @Override
