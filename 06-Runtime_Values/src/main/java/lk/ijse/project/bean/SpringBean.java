14ced5d783868828150996959049cc4995194318
DiilaNa <liyanaarachchidilan@gmail.com>
2025-06-26 13:00:27 +0530

    Runtime Values

--- 06-Runtime_Values/src/main/java/lk/ijse/project/bean/SpringBean.java
+++ 06-Runtime_Values/src/main/java/lk/ijse/project/bean/SpringBean.java
@@ -1,14 +1,15 @@
 package lk.ijse.project.bean;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
+public class SpringBean implements InitializingBean {
 //    public SpringBean() {
 //    }
-    @Autowired(required=false)   /*wedima arguements thina method ek run wnne*/
+  /*  @Autowired(required=false)   *//*wedima arguements thina method ek run wnne*//*
     public SpringBean(@Value("DILAN") String name, @Value("2003") int number, @Value("true") boolean value) {
         System.out.println("SpringBean Constructor :" + name);
         System.out.println("SpringBean Constructor :" + number);
@@ -19,6 +20,18 @@ public class SpringBean {
     public SpringBean(@Value("DILAN") String name, @Value("2003") int number) {
         System.out.println("SpringBean Constructor :" + name);
         System.out.println("SpringBean Constructor :" + number);
+    }*/
+
+    @Value("Dilan")
+    private String name;
+
+    public SpringBean() {
+        System.out.println("SpringBean Constructor");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(name);
     }
 
 
