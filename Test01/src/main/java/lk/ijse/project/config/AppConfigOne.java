6142db1d283d3b120cb915c5d67413a945236c70
DiilaNa <liyanaarachchidilan@gmail.com>
2025-06-26 11:03:47 +0530

    Imported hibernate

--- Test01/src/main/java/lk/ijse/project/config/AppConfigOne.java
+++ Test01/src/main/java/lk/ijse/project/config/AppConfigOne.java
@@ -1,14 +1,12 @@
 package lk.ijse.project.config;
 
 import lk.ijse.project.bean.E;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.project.bean")
 @Import({AppConfig1.class,AppConfig2.class})
+@ImportResource("classpath:hibernate.cfg.xml") /*imported hibernate*/
 public class AppConfigOne {
     @Bean
     public E e() {
