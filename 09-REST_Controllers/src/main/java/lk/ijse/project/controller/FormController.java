20601f94f042a1c0043ed48498f15648063538d2
DiilaNa <liyanaarachchidilan@gmail.com>
2025-07-03 13:44:22 +0530

    Rest Controller-Data Passing using DTO

--- 09-REST_Controllers/src/main/java/lk/ijse/project/controller/FormController.java
+++ 09-REST_Controllers/src/main/java/lk/ijse/project/controller/FormController.java
@@ -1,23 +1,22 @@
 package lk.ijse.project.controller;
 
 import jakarta.servlet.annotation.MultipartConfig;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.project.dto.UserDto;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("form")
-@MultipartConfig
 public class FormController {
 
     @PostMapping("save")
-    public String sayPost(@RequestParam Map<String, String> formData) {
-        for (Map.Entry<String, String> entry : formData.entrySet()) {
+    public String sayPost(@ModelAttribute UserDto userDto) {
+      /*  for (Map.Entry<String, String> entry : formData.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
-        return "save User";
+        return "save User";*/
+        System.out.println(userDto);
+        return "success save User";
     }
 }
